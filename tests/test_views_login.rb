79898969cf9f87ae4328ba256cd18e9401efb0d5
stahnma <stahnma@websages.com>
2010-10-08 15:48:53 -0500

    Commit before rename.

--- tests/test_views_login.rb
+++ tests/test_views_login.rb
@@ -1,21 +1,39 @@
 #require File.expand_path(File.join(File.dirname(__FILE__), *%w[.. .. spec_helper]))
 
-# When hitting the main page,
-#  and you are not logged in,
-#  you should see a login/password prompt
 describe '/index' do 
-  it "should display login prompt if you're not logged in" do 
-  end
-
-  it "should display a password prompt if you are not logged in" do
+  it "should redirect to login if not logged in" do 
   end
 
   it "should display your account information if you are logged in and not an admin" do 
   end
 
-  it "should display an option for admin menu if you are allowed to be an admin and logged in " do 
+  it "should display an option for admin menu if you are allowed to be an admin and logged in" do 
   end
 
-  it "should provide a logout link if you are logged in" do 
+  it "should provide a logout link" do 
+  end
+end
+
+
+describe '/login' do
+  it 'should display a login field' do
+  end
+
+  it 'should display a password field' do
+  end
+
+  it 'should be able to authenticate via certificate' do
+  end
+
+  it 'should display an error if login field is blank' do
+  end
+
+  it 'should offer a password recovery option if you forgot your password' do 
+  end 
+
+  it 'should have a common footer' do 
+  end 
+
+  it 'should have a favicon' do 
   end
 end
