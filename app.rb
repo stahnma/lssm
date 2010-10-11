14ff587d641ddd968ee53d3af4488beebaf72246
stahnma <stahnma@websages.com>
2010-10-11 16:19:36 -0500

    End of day commit.
    Song:
    Artist:
    Album:

--- app.rb
+++ app.rb
@@ -6,12 +6,28 @@ require 'erb'
 enable :sessions
 
 get '/' do 
+  puts session['username']
+  unless session['username']
+    redirect '/login'
+  end
+end
+
+get '/login' do 
   erb :login
 end
 
-
 post '/login' do 
-  p params
+  
+  #p params
   session['username'] = params['login']
-  #session
+  session['password'] = params['password']
+  # perform authentication
+  session.to_s
+end
+
+get '/logout' do
+  session.each  do |k, v|
+    session[k] = nil
+  end
+  session = nil
 end
