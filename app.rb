651535e36ed0aa9ef39404619569fcce5bbd7bd5
Michael Stahnke <stahnma@puppetlabs.com>
2012-06-13 21:41:39 -0700

    Update basic web framework

--- app.rb
+++ app.rb
@@ -5,6 +5,8 @@ require 'erb'
 
 enable :sessions
 
+set :erb, :trim => '-'
+
 get '/' do
   puts session['username']
   unless session['username']
@@ -19,9 +21,16 @@ end
 post '/login' do
   #p params
   session['username'] = params['login']
+#  :username = params['login']
   session['password'] = params['password']
+#  :password = params['password']
   # perform authentication
-  session.to_s
+  # session.to_s
+  redirect '/:username/edit'
+end
+
+get "/:username/edit" do
+  # find my ldap record
 end
 
 get '/logout' do
